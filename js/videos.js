7f0d05dea11066d4bc92dc56827021c73bedbaf3
senbeyxd <nicci250404@gmail.com>
2022-05-20 22:41:22 +0200

    new videos

--- js/videos.js
+++ js/videos.js
@@ -2,17 +2,10 @@ const videos = [
   { path: "keep-my-coo", name: "Keep My Coo", artists: ["Lil Peep"] },
   { path: "live-forever", name: "Live Forever", artists: ["Lil Peep"] },
   { path: "gym-class", name: "Gym Class", artists: ["Lil Peep"] },
-  { path: "the-brightside", name: "The Brightside", artists: ["Lil Peep"] },
   {
     path: "hollywood-dreaming",
     name: "Hollywood Dreaming",
     artists: ["Lil Peep", "Gab3"],
-  },
-  { path: "backseat", name: "Backseat", artists: ["Lil Peep", "Lil Tracy"] },
-  {
-    path: "white-tee",
-    name: "White Tee",
-    artists: ["Lil Peep", "Lil Tracy"],
   },
   { path: "beamer-boy", name: "beamer boy", artists: ["Lil Peep", "Nedarb"] },
   {
@@ -21,13 +14,7 @@ const videos = [
     artists: ["Lil Peep", "Nedarb"],
   },
   { path: "2nd-hand", name: "2nd Hand", artists: ["$uicideboy$"] },
-  { path: "o-pana", name: "O Pana!", artists: ["$uicideboy$"] },
   { path: "face-it", name: "Face It", artists: ["$uicideboy$"] },
-  {
-    path: "rag-round-my-skull",
-    name: "Rag Round My Skull",
-    artists: ["$uicideboy$"],
-  },
   {
     path: "for-the-last-time",
     name: "For the Last Time",
@@ -46,9 +33,38 @@ const videos = [
     artists: ["t-low", "Sevi Rin", "Heinie Nüchtern"],
   },
   {
-    path: "we-made-it",
-    name: "We Made It",
-    artists: ["t-low", "Miksu / Macloud"],
+    path: "packs-auf-mir",
+    name: "Packs auf mir",
+    artists: ["102 Boyz", "65Goonz", "Chapo102", "Addikt102"],
+  },
+  {
+    path: "jungs-mit-rueckgrat",
+    name: "Jungs mit Rückgrat",
+    artists: ["102 Boyz", "Chapo102", "Stacks102"],
+  },
+  {
+    path: "heineken-emblem",
+    name: "Heineken Emblem",
+    artists: [
+      "102 Boyz",
+      "Chapo102",
+      "Skoob102",
+      "KKuba102",
+      "Addikt102",
+      "Duke102",
+      "Stacks102",
+    ],
+  },
+  {
+    path: "uber-xl",
+    name: "Uber XL",
+    artists: ["Addikt102", "Stacks102", "102 Boyz"],
+  },
+  { path: "mr-x", name: "Mr. X", artists: ["Lucio101"] },
+  {
+    path: "bunte-flavours",
+    name: "Bunte Flavours",
+    artists: ["Lucio101", "Nizi19", "Omar101"],
   },
   {
     path: "powerade",
